e7da7ab8dcf8d36ebb8fec6bdecfc7ccd93c61ca
Shaquille Demsi <shaquille.demsi@yahoo.com>
2017-02-07 11:30:33 +0700

    update comment in api request

--- application/models/M_Forecast.php
+++ application/models/M_Forecast.php
@@ -5,6 +5,8 @@ class M_Forecast extends CI_Model
     function current_weather($city)
     {
 		 //get JSON
+	    	 //you can get param appid from your account in openweathermap.org
+	    	 //http://api.openweathermap.org/data/2.5/weather?appid=[YOUR_APP_ID]&units=metric&q=$city
 		 $json = file_get_contents("http://api.openweathermap.org/data/2.5/weather?appid=770a17f9520e41124656aa601bc34b3c&units=metric&q=$city", false);
 
 		 //decode JSON to array
@@ -17,6 +19,8 @@ class M_Forecast extends CI_Model
     function forecast_weather($city)
     {
 		 //get JSON
+	    	 //you can get param appid from your account in openweathermap.org
+	    	 //http://api.openweathermap.org/data/2.5/forecast/daily?appid=[YOUR_APP_ID]&units=metric&q=$city&cnt=7
 		 $json = file_get_contents("http://api.openweathermap.org/data/2.5/forecast/daily?appid=770a17f9520e41124656aa601bc34b3c&units=metric&q=$city&cnt=7", false);
 
 		 //decode JSON to array
